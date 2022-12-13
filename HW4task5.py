ac980af2625acf13455a19c57d3cc3d387b7f904
Anna <anna.gloriole@gmail.com>
2022-12-13 13:57:16 +0300

    HW4

--- HW4task5.py
+++ HW4task5.py
@@ -4,25 +4,8 @@
 # Файл2: 41*x^3 + 6*x² + 2*x + 98
 # Вывод Файл3: 41*x^3 + 8*x^2 + 6*x + 103
 
-
-# list = input("please enter elemets use split\n").split()
-
-# # i = 0
-# # while (i < len(list)):
-#     if x
-#     elif x^2
-#     elif x^3
-
-# #     numb = len(list) - i - 1
-# #     list2.append(int(list[i]) * int(list[numb]))
-# #     i+=1
-# print(list)
-
-
-
 def p1(x):
     return 2*x**2 + 4*x + 5
-
 
 data = open('file1.txt', 'a')
 data.writelines(p1)
@@ -45,6 +28,3 @@ exit()
 data = open('file3.txt', 'a')
 data.writelines(p2+p1)
 data.close()
-
-# with open('file3.txt', 'a') as data:
-#     data.writelines(polynomial1 + polynomial2)
